06ef41116422c79e127d43f06f2af8f1c9111474
ip2211 <praneeth9th@gmail.com>
2022-10-02 17:35:02 +0530

    commit done

---
+++ banking_application/src/banking_application/acc_details.java
@@ -0,0 +1,17 @@
+package banking_application;
+public class acc_details {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account customerAccount1 = new Account("ip", "22");
+
+		
+		
+//		Account beyonce = new Account("Beyonce", "B00002");
+		customerAccount1.showMenu();
+		
+		
+	}
+
+}
